78c6db0be15c3f56496ea88611bf9277a372f536
mzaka7 <m.zaka7@gmail.com>
2018-07-12 19:02:30 +0400

    Routing Module added

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,18 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "my-app",
-  template: `<Label text='Hello coffee world'></Label> 
-  <Image src="~/images/apple.jpg"></Image>
-  `,
-  styles: [`
-    @keyframes spin {
-      from { transform: rotate(0); } to { transform: rotate(360); }
-    }
-    Image {
-      animation-name: spin; animation-duration: 1s;
-      animation-iteration-count: infinite;
-      animation-timing-function: linear;
-    }
-  `]
+  templateUrl: "app.component.html",
+  styles: []
 })
 export class AppComponent {}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { AppRoutingModule } from "./app.routing";
 
 import { AppComponent } from "./app.component";
 
+import {HomeComponent} from "./pages/home/home.component"
+
 @NgModule({
-  imports: [NativeScriptModule],
-  declarations: [AppComponent],
+  imports: [NativeScriptModule,AppRoutingModule],
+  declarations: [AppComponent,HomeComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
