3c39ad1115ce1d604adfca356a82cf2bc70d4e9d
Chakir Qatab <javachakir@gmail.com>
2018-07-12 13:43:55 +0400

    first commit

---
+++ app/shared/product/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ProductService {
+
+  constructor() { }
+
+  getCategories(level:number){
+
+  }
+  getProducts(category:number){
+    
+  }
+
+
+}
---
+++ app/shared/shop/shop.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Product } from '~/shared/product/product';
+
+@Injectable()
+export class ShopService {
+
+  constructor() { }
+
+  getShops(){
+
+  }
+  getShopsNearme(){
+
+  }
+  getShopsHavingProduct(product:Product){
+
+  }
+
+
+}
